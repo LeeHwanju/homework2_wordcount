690a60341ed1263e36b67499995e41938754cecd
LeeHwanju <hwanju0530@likelion.org>
2019-06-11 21:43:39 +0900

    homework2_wordcount

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\이환주\desktop\멋쟁이 사자처럼 7기\practice2_wordcount2\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
